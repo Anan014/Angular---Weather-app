9e3cb599a8b14c39fa4d764aac15a4325caade6a
Anan <ananamer95@gmail.com>
2022-07-23 23:57:43 +0300

    adding env

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -9,19 +9,23 @@ import { list, Weather } from '../interfaces/weather.interface';
 })
 export class WeatherService {
 
+  weatherApiUrl = 'https://community-open-weather-map.p.rapidapi.com/forecast'
+  XRapidAPIKeyHeaderValue = 'ad5140e520msh0a784ed64c1738cp1a41f3jsn13b4dd1fd608'
+  XRapidAPIHostHeaderValue = 'community-open-weather-map.p.rapidapi.com'
+
   constructor(private http: HttpClient) { }
   locationInput = 'Israel';
   listArrayIndex: number = 0;
   weatherList: list[] = [];
   allWeatherObject: Weather = {} as Weather;
-  sortFocus:string ='dt';
+  sortFocus: string = 'dt';
   lastInput = 'Israel';
 
   getWeather(location: string) {
-    return this.http.get<Weather>(environment.weatherApiUrl, {
+    return this.http.get<Weather>(this.weatherApiUrl, {
       headers: new HttpHeaders()
-        .set('X-RapidAPI-Key', environment.XRapidAPIKeyHeaderValue)
-        .set('X-RapidAPI-Host', environment.XRapidAPIHostHeaderValue),
+        .set('X-RapidAPI-Key', this.XRapidAPIKeyHeaderValue)
+        .set('X-RapidAPI-Host', this.XRapidAPIHostHeaderValue),
       params: new HttpParams()
         .set('q', location)
         .set('units', 'metric')
