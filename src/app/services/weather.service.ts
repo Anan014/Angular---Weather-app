b7efdccb8a4b38f11307835c9d5de5c015c43074
Anan014 <89395304+Anan014@users.noreply.github.com>
2022-08-01 14:53:41 +0300

    updated http request URL
    Updated HTTP request URL to Open Weather API instead of Rapid API

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -9,23 +9,38 @@ import { list, Weather } from '../interfaces/weather.interface';
 })
 export class WeatherService {
 
+  weatherApiUrl = 'https://community-open-weather-map.p.rapidapi.com/forecast'
+  XRapidAPIKeyHeaderValue = 'ad5140e520msh0a784ed64c1738cp1a41f3jsn13b4dd1fd608'
+  XRapidAPIHostHeaderValue = 'community-open-weather-map.p.rapidapi.com'
+
+  open_Weather_API_Url = 'https://api.openweathermap.org/data/2.5/forecast?'
+
   constructor(private http: HttpClient) { }
   locationInput = 'Israel';
   listArrayIndex: number = 0;
   weatherList: list[] = [];
   allWeatherObject: Weather = {} as Weather;
-  sortFocus:string ='dt';
+  sortFocus: string = 'dt';
   lastInput = 'Israel';
 
+  // getWeather(location: string) {
+  //   return this.http.get<Weather>(this.weatherApiUrl, {
+  //     headers: new HttpHeaders()
+  //       .set('X-RapidAPI-Key', this.XRapidAPIKeyHeaderValue)
+  //       .set('X-RapidAPI-Host', this.XRapidAPIHostHeaderValue),
+  //     params: new HttpParams()
+  //       .set('q', location)
+  //       .set('units', 'metric')
+  //       .set('mode', 'json')
+  //   })
+  // }
+
   getWeather(location: string) {
-    return this.http.get<Weather>(environment.weatherApiUrl, {
-      headers: new HttpHeaders()
-        .set('X-RapidAPI-Key', environment.XRapidAPIKeyHeaderValue)
-        .set('X-RapidAPI-Host', environment.XRapidAPIHostHeaderValue),
+    return this.http.get<Weather>(this.open_Weather_API_Url, {
       params: new HttpParams()
         .set('q', location)
+        .set('appid', 'ad74905d96e028b53c9809aea1ca4261')
         .set('units', 'metric')
-        .set('mode', 'json')
     })
   }
 }
